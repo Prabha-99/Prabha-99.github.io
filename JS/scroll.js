763c84077844762592065baa7a0ee902365e17fd
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-03-04 22:47:46 +0530

    last few modifications

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,12 +1,18 @@
 var navbar = document.getElementById("navbar");
+var brand =document.getElementById("brand");
   var navbarHeight = 644;
 
   window.onscroll = function() {
     if (window.pageYOffset > navbarHeight) {
       navbar.classList.remove("bg-transparent");
-      navbar.classList.add("bg-dark");
+      navbar.style.backgroundColor = "#322c23";
+      // navbar.classList.add("bg-dark");
+      brand.style.color="#ffd900";
+      
     } else {
       navbar.classList.remove("bg-dark");
       navbar.classList.add("bg-transparent");
+      brand.style.color="#322c23";
+      
     }
   };
