86dad1f6cef156263daa455497362ab543782905
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2024-05-01 13:30:48 +0530

    Some landing changes

--- JS/scroll.js
+++ JS/scroll.js
@@ -7,18 +7,18 @@ var navLinks = document.querySelectorAll(".nav-link"); // select all nav-link el
   window.onscroll = function() {
     if (window.pageYOffset > navbarHeight) {
       navbar.classList.remove("bg-transparent"); //Removing the Transparency when scrolling down
-      navbar.style.backgroundColor = "#332D2D"; //Setting background color of navbar when scrolling down
-      brand.style.color="#9754CB"; //Change the nav-brand color when scrolling to down
+      navbar.style.backgroundColor = "#333B60"; //Setting background color of navbar when scrolling down
+      brand.style.color="#ffffff"; //Change the nav-brand color when scrolling to down
 
       // Updating the color of all nav-links to white when scrolls down.
         for(var i=0; i<navLinks.length; i++) {
-            navLinks[i].style.color = "#9754CB";
+            navLinks[i].style.color = "#ffffff";
         }
       
     } else {
       navbar.classList.remove("bg-dark");
       navbar.classList.add("bg-transparent");
-      brand.style.color="#9754CB";
+      brand.style.color="#ffffff";
       
       // Updating the color of all nav-links to their original color
         for(var i=0; i<navLinks.length; i++) {
