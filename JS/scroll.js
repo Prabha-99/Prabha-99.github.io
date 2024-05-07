a30a37de7bba36a32d96c784aa84df66415344b7
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2024-05-07 09:37:27 +0530

    Added some customizations

--- JS/scroll.js
+++ JS/scroll.js
@@ -2,13 +2,14 @@ var navbar = document.getElementById("navbar");
 var brand =document.getElementById("brand");
 var navLinks = document.querySelectorAll(".nav-link"); // select all nav-link elements
 
-  var navbarHeight = 644;
+  var navbarHeight = 144;
 
   window.onscroll = function() {
     if (window.pageYOffset > navbarHeight) {
       navbar.classList.remove("bg-transparent"); //Removing the Transparency when scrolling down
       navbar.style.backgroundColor = "#333B60"; //Setting background color of navbar when scrolling down
       brand.style.color="#ffffff"; //Change the nav-brand color when scrolling to down
+
 
       // Updating the color of all nav-links to white when scrolls down.
         for(var i=0; i<navLinks.length; i++) {
@@ -27,3 +28,9 @@ var navLinks = document.querySelectorAll(".nav-link"); // select all nav-link el
       
     }
   };
+
+
+  
+      var copy = document.querySelector(".logos-slide").cloneNode(true);
+      document.querySelector(".logos").appendChild(copy);
+
